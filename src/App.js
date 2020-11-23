d1db9976d76fd23e2944a5c91dd45088632625c9
nanpena <peangtan.pena@gmail.com>
2020-11-23 17:04:40 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Movie Search </h1>
+      <input/>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default App;
